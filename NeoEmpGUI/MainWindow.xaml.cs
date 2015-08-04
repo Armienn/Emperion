61df27b6e9f2308764c083822b4f1f1d5315c460
Armienn <nigathr@gmail.com>
2015-08-04 12:36:06 +0200

    Added references

--- NeoEmpGUI/MainWindow.xaml.cs
+++ NeoEmpGUI/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NeaKit.Geometry2D.Hex;
+using NeoEmperion;
 
 namespace NeoEmpGUI {
 	/// <summary>
